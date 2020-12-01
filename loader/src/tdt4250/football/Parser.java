c9ab5bb3c0f2418a4caf60541f0cc7353efbb9b8
Einar Sønju <einarsonju@gmail.com>
2020-12-01 16:17:53 +0100

    Add docstrings to Parser methods + some renaming

--- loader/src/tdt4250/football/Parser.java
+++ loader/src/tdt4250/football/Parser.java
@@ -1,13 +1,13 @@
 package tdt4250.football;
 import java.io.IOException;
+import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import tdt4250.football.json.folder.Player;
+import tdt4250.football.json.folder.Team;
 
 public class Parser {
 	
@@ -19,14 +19,31 @@ public class Parser {
 		defaultObjectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		return defaultObjectMapper;
 	}
+
+	/**
+	 * Parse a JSON string into a single JsonNode object
+	 *
+	 * @param src JSON string
+	 * @return JsonNode
+	 * @throws IOException
+	 */
 	public static JsonNode parse(String src) throws IOException{
 		return objectMapper.readTree(src);
 	}
-	
-	public static <A> A fromJson(JsonNode node ,Class<A> pojo) throws JsonProcessingException{
-		return objectMapper.treeToValue(node, pojo);
+
+	/**
+	 * Converts a JsonNode into a single Java object
+	 *
+	 * @param <A> object type
+	 * @param node
+	 * @param dataClass
+	 * @return
+	 * @throws JsonProcessingException
+	 */
+	public static <A> A fromJson(JsonNode node ,Class<A> dataClass) throws JsonProcessingException{
+		return objectMapper.treeToValue(node, dataClass);
 	}
-	
+
 	public static void populatePojos() {
 		
 	}
