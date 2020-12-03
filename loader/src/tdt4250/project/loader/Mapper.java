f00415498bd4dcc79393f5dc9f752c4160d63af5
Øystein Bjørkeng Haugen <oystebha@stud.ntnu.no>
2020-12-03 15:58:07 +0100

    added mapper for match, added mapper for single team

--- loader/src/tdt4250/project/loader/Mapper.java
+++ loader/src/tdt4250/project/loader/Mapper.java
@@ -6,9 +6,12 @@ import java.util.List;
 import TDT4250.Project.league.League;
 import TDT4250.Project.league.LeagueFactory;
 import TDT4250.Project.league.LeaguePackage;
+import TDT4250.Project.league.Match;
 import TDT4250.Project.league.Season;
 import TDT4250.Project.league.Team;
+import tdt4250.project.loader.data.json.BookingData;
 import tdt4250.project.loader.data.json.LeagueData;
+import tdt4250.project.loader.data.json.MatchData;
 import tdt4250.project.loader.data.json.SeasonData;
 import tdt4250.project.loader.data.json.TeamData;
 
@@ -16,19 +19,28 @@ import tdt4250.project.loader.data.json.TeamData;
  * Maps data objects to model instance objects
  */
 public class Mapper {
+	
+	public static Team mapTeam(TeamData teamData) {
+		Team team = getLeagueFactory().createTeam();
+		
+		team.setName(teamData.name);
+		team.setAbbr(teamData.abbreviation);
+		team.setStadium(teamData.stadium);
+		
+		return team;
+	}
+	
 	public static List<Team> mapTeams(List<TeamData> teamData) {
 		List<Team> teams = new ArrayList<>();
 
 		for (TeamData td: teamData) {
-			Team team = getLeagueFactory().createTeam();
-			team.setName(td.name);
-			team.setAbbr(td.abbreviation);
-			team.setStadium(td.stadium);
+			Team team = mapTeam(td);
 			teams.add(team);
 		}
 
 		return teams;
 	}
+	
 
 	public static League mapLeague(LeagueData leagueData) {
 		League league = getLeagueFactory().createLeague();
@@ -46,6 +58,21 @@ public class Mapper {
 		season.setName(seasonData.name);
 
 		return season;
+	}
+	
+	public static Match mapMatch(MatchData matchData) {
+		Match match = getLeagueFactory().createMatch();
+		match.setHometeam(mapTeam(matchData.getHometeam()));
+		match.setAwayteam(mapTeam(matchData.getAwayteam()));
+		
+		for (BookingData bd : matchData.getBookings()) {
+			System.out.println("skrt");
+		}
+		match.setHomegoals(matchData.homeGoals);
+		match.setAvaygoals(matchData.awayGoals);
+		match.setReferee(matchData.referee);
+		
+		return match;
 	}
 
 	private static LeagueFactory leagueFactory;
